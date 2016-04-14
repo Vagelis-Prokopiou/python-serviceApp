65f1f74448c332d822617b13bbe62a2b13ce33ef
Vagelis Prokopiou <drz4007@gmail.com>
2016-04-14 18:48:07 +0300

    More work

--- service.py
+++ service.py
@@ -133,22 +133,81 @@ def main():
                 # dateChanged = l[1]
                 # dateInterval = l[2]
 
+                # Create the global mileage of the vehicle variable.
+            while True:
+                current_kms = input('Please, provide the current kilometers/miles of the vehicle: ')
+                if validate_kms(current_kms):
+                    current_kms = int(current_kms)
+                    break
+                else:
+                    print('The value you provide is not right. Please, try again.\n')
+
                 # Check the time that has past.
-                if compare_dates(dateChanged, today, dateInterval):
-                    messages.append(
-                        'You have exceeded the allowed {0} months between {1} changes. You must change the {1} again now!'.format(
-                            (l[2].lower()), l[0].lower()))
+                # if compare_dates(dateChanged, today, dateInterval):
+                #     messages.append(
+                #         'You have exceeded the allowed {0} months between {1} changes. You must change the {1} again now!'.format(
+                #             (l[2].lower()), l[0].lower()))
 
                 # Check how many kilometers have past since the last change.
-                if compare_mileage(current_kms, l[3], l[4]):
-                    messages.append(
-                        'You have exceeded the allowed {0} kms between {1} changes. You must change the {1} again now!'.format(
-                            (l[4]).lower(), l[0].lower()))
+                # if compare_mileage(current_kms, l[3], l[4]):
+                #     messages.append(
+                #         'You have exceeded the allowed {0} kms between {1} changes. You must change the {1} again now!'.format(
+                #             (l[4]).lower(), l[0].lower()))
 
-            file.close()
             break
         elif userChoise == int(1):
             print('Update an existing data entry.')
+            # Check if the user wants to update anything.
+            while True:
+                data_update = input('\nIf you have made any servicing to your vehicle\n'
+                                    'and you want to update the data, choose the right spare part\n'
+                                    'according to the following table, otherwise, just press "Enter".\n\n'
+                                    'For "Spark" press 1.\n'
+                                    'For "Oil" press 2.\n'
+                                    'For "Oil filter" press 3.\n'
+                                    'For "Air filter" press 4.\n'
+                                    'Enter your choice": ', )
+                if data_update == '':
+                    break
+                elif (int(data_update) > len(sparePartsDict)) or (int(data_update) <= 0):
+                    print('\nYour choice seems wrong. Please, try again.\n\n')
+                else:
+                    break
+
+            if data_update != '':
+                while True:
+                    user_date = input('Please, provide the date of the ' + sparePartsDict[
+                        data_update].lower() + ' change (e.g. 31/12/2015): ')
+                    m = re.search(
+                        "^(((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))$",
+                        user_date)
+                    if m == None:
+                        print('\nThe date you provided is not valid. Please try again.\n')
+                    else:
+                        # Check if the date provided is within a logical time span from today.
+                        if validate_date(user_date, today) == 1:
+                            print(
+                                '\nThe date you provided lies ahead in the future.'
+                                '\nI cannot accept that, unless you are some king of prophet, or unless you own a time machine.\n')
+                        elif validate_date(user_date, today) == -1:
+                            print('\nThe date you provided seems too old. I just doesn\'t make sense...\n')
+                        else:
+                            break
+
+                while True:
+                    user_kms = input('\nPlease, provide the kilometers of the ' + sparePartsDict[
+                        data_update].lower() + ' change: ')
+                    if validate_kms(user_kms):
+                        user_kms = int(user_kms)
+                        if user_kms <= current_kms:
+                            break
+                        else:
+                            print(
+                                '\nThe kilometers you provided are more than the total kilometers of the vehicle. Something is terribly wrong...\n')
+
+                # If all the above, update the data.
+                update(sparePartsDict[data_update], user_date, user_kms)
+                # End of function.
             break
         elif userChoise == int(2):
             print('Insert a new data entry.')
@@ -162,66 +221,9 @@ def main():
 
 
 
-    # Create the global mileage of the vehicle variable.
-    while True:
-        current_kms = input('Please, provide the current kilometers/miles of the vehicle: ')
-        if validate_kms(current_kms):
-            current_kms = int(current_kms)
-            break
-        else:
-            print('The value you provide is not right. Please, try again.\n')
 
-    # Check if the user wants to update anything.
-    while True:
-        data_update = input('\nIf you have made any servicing to your vehicle\n'
-                            'and you want to update the data, choose the right spare part\n'
-                            'according to the following table, otherwise, just press "Enter".\n\n'
-                            'For "Spark" press 1.\n'
-                            'For "Oil" press 2.\n'
-                            'For "Oil filter" press 3.\n'
-                            'For "Air filter" press 4.\n'
-                            'Enter your choice": ', )
-        if data_update == '':
-            break
-        elif (int(data_update) > len(sparePartsDict)) or (int(data_update) <= 0):
-            print('\nYour choice seems wrong. Please, try again.\n\n')
-        else:
-            break
 
-    if data_update != '':
-        while True:
-            user_date = input('Please, provide the date of the ' + sparePartsDict[
-                data_update].lower() + ' change (e.g. 31/12/2015): ')
-            m = re.search(
-                "^(((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))$",
-                user_date)
-            if m == None:
-                print('\nThe date you provided is not valid. Please try again.\n')
-            else:
-                # Check if the date provided is within a logical time span from today.
-                if validate_date(user_date, today) == 1:
-                    print(
-                        '\nThe date you provided lies ahead in the future.'
-                        '\nI cannot accept that, unless you are some king of prophet, or unless you own a time machine.\n')
-                elif validate_date(user_date, today) == -1:
-                    print('\nThe date you provided seems too old. I just doesn\'t make sense...\n')
-                else:
-                    break
 
-        while True:
-            user_kms = input('\nPlease, provide the kilometers of the ' + sparePartsDict[
-                data_update].lower() + ' change: ')
-            if validate_kms(user_kms):
-                user_kms = int(user_kms)
-                if user_kms <= current_kms:
-                    break
-                else:
-                    print(
-                        '\nThe kilometers you provided are more than the total kilometers of the vehicle. Something is terribly wrong...\n')
-
-        # If all the above, update the data.
-        update(sparePartsDict[data_update], user_date, user_kms)
-        # End of function.
 
 
 
